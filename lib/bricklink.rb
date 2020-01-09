38cb7b53d9470f59fd6c3f1503bc3995c023fb84
dbwinger <dbwinger@gmail.com>
2020-01-09 12:36:17 -0500

    Use Bundler for gem init

--- lib/bricklink.rb
+++ lib/bricklink.rb
@@ -2,3 +2,7 @@ require 'dotenv/load'
 require 'byebug' if ENV['ENV'] == 'development'
 require 'bricklink/version'
 require 'bricklink/client'
+
+module BrickLink
+  class Error < StandardError; end
+end
---
+++ spec/bricklink_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Bricklink do
+  it "has a version number" do
+    expect(Bricklink::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
